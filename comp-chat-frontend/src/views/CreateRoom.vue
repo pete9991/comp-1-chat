524906599cce34c98e44f4663b606f5ae54542dc
pete9991 <peter.lydeking@gmail.com>
2022-03-14 08:16:04 +0100

    worked on create room, and refactored front end a bit

--- comp-chat-frontend/src/views/CreateRoom.vue
+++ comp-chat-frontend/src/views/CreateRoom.vue
@@ -1,9 +1,15 @@
-<template></template>
+<template>
+  <h2>Here you can create a room</h2>
+  <input v-model="inputName" placeholder="Enter room name" />
+  <button @click="handleCreateRoom">Create</button>
+</template>
 
-<script>
-export default {
-  name: "CreateRoom",
-};
+<script setup lang="ts">
+import { ref } from "vue";
+
+const inputName = ref("");
+
+function handleCreateRoom() {}
 </script>
 
 <style scoped></style>
